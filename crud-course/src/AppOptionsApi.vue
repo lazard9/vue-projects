728e809d0d4c963f0fd6cbbcf342d244b37c3f57
Lazar Dacic <lazar.dacic@gmail.com>
2025-06-05 02:08:12 +0200

    feat: options API example

---
+++ crud-course/src/AppOptionsApi.vue
@@ -0,0 +1,31 @@
+<script>
+    export default {
+        data: () => ({
+            name: 'Options API',
+            status: 'pending',
+        }),
+        methods: {
+            changeStatus() {
+                if (this.status === 'active') {
+                    this.status = 'pending';
+                } else if (this.status === 'pending') {
+                    this.status = 'inactive';
+                } else {
+                    this.status = 'active';
+                }
+                
+            },
+        },
+    };
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+
+    <p v-if="status === 'active'">Status: {{ status }}</p>
+    <p v-else-if="status === 'pending'">Status: {{ status }}</p>
+    <p v-else="status === 'inactive'">Status: {{ status }}</p>
+    
+    <!-- <button v-on:click="changeStatus">Change Status</button> -->
+    <button @click="changeStatus">Change Status</button>
+</template>
